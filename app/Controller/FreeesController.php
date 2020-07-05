5b1838d541b41c93993c148f800e788c8c928911
Nash-BEAT <nash.0cal@gmail.com>
2020-07-06 01:40:47 +0900

    viewの作成とjqueryの記述

---
+++ app/Controller/FreeesController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+App::uses('AppController', 'Controller');
+
+class FreeesController extends AppController {
+
+/**
+ * This controller does not use a model
+ *
+ * @var array
+ */
+	public $uses = array();
+
+	public function post() {
+
+	}
+
+	public function bill (){
+
+	}
+}
